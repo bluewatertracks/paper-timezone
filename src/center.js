bb13859b2007c7e17d85e423df202e1b9f51b9dc
bhargavkonkathi <bhargav.konkathi@gmail.com>
2017-02-01 17:36:55 +0530

    Included extra properties.Changed file structure.

---
+++ src/center.js
@@ -0,0 +1,16 @@
+function Center(data, scope) {
+    var name = data.name;
+    var x = (180 + data.long) / 360;
+    var y = (90 - data.lat) / 180;
+    var span = document.createElement('span');
+    span.style.left = x * 100 + '%';
+    span.style.top = y * 100 + '%';
+    span.setAttribute("name", name);
+    var dom = scope.$.inset.appendChild(span);
+    var distSqr = function(xNew, yNew) {
+        var dx = x - xNew,
+            dy = y - yNew;
+        return dx * dx + dy * dy;
+    };
+    return {name:name, x:x,y:y,dom:dom,distSqr: distSqr};
+}
